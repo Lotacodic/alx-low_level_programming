870e781ba7aab79c5f33a8b17a217f1d931c0929
Lotacodic <ngenejoshua05@gmail.com>
2022-04-07 19:32:02 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main: Entry point
- * Return: 0 is it exited properly, non-zero otherwise
- */
+* main: Entry point
+* Return: 0 is it exited properly, non-zero otherwise
+*
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
