e82b2a2cd24c11d8397364698981e30f63866885
Lotacodic <ngenejoshua05@gmail.com>
2022-04-07 21:29:45 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* * * main: Entry point
-* * *
-* * * return: 0 if exited, non-zero if otherwise
+ *  *  * main - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
 */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
