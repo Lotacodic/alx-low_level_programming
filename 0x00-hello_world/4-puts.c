3872c421c4ed496cc6eb620dce9c3fbf8b3df383
Lotacodic <ngenejoshua05@gmail.com>
2022-04-07 20:25:33 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* main: Entry point
+* main - Entry point
 * puts: Prints "Programming is like building a multilingual puzzle."
 * Return: 0 is it exited properly, non-zero otherwise
 *
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
