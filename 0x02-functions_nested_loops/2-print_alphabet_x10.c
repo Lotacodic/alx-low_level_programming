a47044224894307fd8c017577a6228aab7056a58
Lotacodic <ngenejoshua05@gmail.com>
2022-04-12 03:26:53 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+	int n, en;
+
+	en = 0;
+
+	while (en < 10)
+	{
+		for (n = 97; n <= 122; n++)
+		{
+			_putchar(n);
+		}
+		en++;
+		_putchar('\n');
+
+	}
+	return (0);
