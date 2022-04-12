430b88a20f416c5921d3f871c5d681b69f43244a
Lotacodic <ngenejoshua05@gmail.com>
2022-04-12 09:02:48 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- * print_alphabet - Prints the alphabet in lowercase.
- *
- * Return: Always 0 (Success)
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
-	char i;
+        char alpha;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-	_putchar(i);
-	}
-	_putchar('\n');
+        for (alpha = 'a'; alpha <= 'z'; alpha++)
+        _putchar(alpha);
+
+        _putchar('\n');
 }
