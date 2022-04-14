ec88b391e0313965cecc7d5279ae00df6868773d
Lotacodic <ngenejoshua05@gmail.com>
2022-04-14 15:49:16 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main.h: Write a function that checks for uppercase character.
+ * return: returns 1 if c is uppercase and returns 0 otherwise.
+*/
+ int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
