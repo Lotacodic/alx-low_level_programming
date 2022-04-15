e82fd938efb8200e59d82bccc076350f327ddc12
Lotacodic <ngenejoshua05@gmail.com>
2022-04-14 17:33:30 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/**
+/***
  * _isupper- Write a function that checks for uppercase character.
  * @c: character to be checked
  * return: returns 1 if c is uppercase and returns 0 otherwise.
@@ -7,7 +7,6 @@
 int _isupper(int c)
 {
 	char a;
-
 	for (a = 'A'; a <= 'Z'; a++)
 	{
 		if (c == a)
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,7 @@
+#include<unistd.h>                                                                                                                              
+                                                                                                                                                
+int _putchar(a)                                                                                                                                 
+{                                                                                                                                               
+write(1, &a, 1);                                                                                                                                 
+return (0);                                                                                                                                     
+}
