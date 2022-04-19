85bf1b41ad1d5bce5e41f92a9b700f4bac5e8a5c
Lotacodic <ngenejoshua05@gmail.com>
2022-04-19 13:48:22 -0700

    task 3

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,13 @@
 */
 int _isupper(int c)
 {
-char i;
-for (i = 'A'; i <= 'Z'; i++)
-{
+	char i;
+	for (i = 'A'; i <= 'Z'; i++)
+	{
 	if (c == i)
 	{
 		return (1);
 	}
-}               
+	}               
                 return (0);
 }
