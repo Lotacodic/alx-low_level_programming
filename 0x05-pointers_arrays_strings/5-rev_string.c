7a6647dd16c436956002c662c244ec90e49d1010
Lotacodic <ngenejoshua05@gmail.com>
2022-04-20 13:49:02 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: void
+*/
+void rev_string(char *s)
+{
+	int a, b, c, d;
+	char str[a];
+
+	a = 0;
+	c = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	for (b = a - 1; b >= 0; b--)
+	{
+		str[c] = s[b];
+		c++;
+	}
+	for (d = 0; d < a; d++)
+	{
+		s[d] = str[d];
+	}
+}
