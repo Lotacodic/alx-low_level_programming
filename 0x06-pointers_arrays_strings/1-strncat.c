8596082dab035c15d17b0d7166741d787a29ac05
Lotacodic <ngenejoshua05@gmail.com>
2022-04-24 14:47:51 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,28 +1,30 @@
 #include "main.h"
-
 /**
- * _strncpy - copies a string
- * @src: second string to copy from
- * @dest: string to be overwritten
- * @n: number of values to copy
- * Description: copies string
- * Return: pointer to dest
- **/
-
-char *_strncpy(char *dest, char *src, int n)
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
+	int j;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest[i] = src[i];
-	}
-
-	while (i < n)
-	{
-		dest[i] = '\0';
 		i++;
 	}
-
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
